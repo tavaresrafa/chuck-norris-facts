6bf7e71ad9e8bbcbc1b3b7d57e247256123202cf
Rafael Tavares <rafael.sousat@gmail.com>
2020-08-31 18:21:56 -0300

    Updates

---
+++ precache-manifest.e6de42df8cf15839c507e6c24488dcac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d5bc7892e18b8dc9a8758335a15cb20",
+    "url": "/react-chuck-norris-facts/index.html"
+  },
+  {
+    "revision": "5d3f6949a756d1df2259",
+    "url": "/react-chuck-norris-facts/static/js/2.604016fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-chuck-norris-facts/static/js/2.604016fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9879a72b7d4032d6919f",
+    "url": "/react-chuck-norris-facts/static/js/main.887074ec.chunk.js"
+  },
+  {
+    "revision": "790c9e38bf6892717849",
+    "url": "/react-chuck-norris-facts/static/js/runtime-main.370e996a.js"
+  },
+  {
+    "revision": "d2080adfd42893acaa03dca34bd87c73",
+    "url": "/react-chuck-norris-facts/static/media/chucknorris_logo.d2080adf.png"
+  }
+]);
